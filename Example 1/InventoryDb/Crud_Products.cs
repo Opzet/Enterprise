2053d1859ac9016f853945c74f78443d79f84dd8
David iMac <david@digitalgeo.com.au>
2023-11-26 10:58:11 +0800

    WIP

---
+++ Example 1/InventoryDb/Crud_Products.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Runtime.Remoting.Contexts; // Make sure you have this for EntityState
+
+namespace InventoryDb
+{
+    public static class Crud_Products
+    {
+        // Db Create/ Update
+        public static void CreateUpdate(Product product)
+        {
+            using (var db = new Db())
+            {
+                if (product.Id == -1) // Assuming -1 is an unsaved product
+                {
+                    db.Products.Add(product);
+                }
+                else
+                {
+                    db.Entry(product).State = EntityState.Modified;
+                }
+
+                db.SaveChanges();
+            }
+        }
+
+        // Db Read one
+        public static Product GetProduct(long id)
+        {
+            if (id == -1)
+                return new Product();
+
+            using (var db = new Db())
+            {
+                return db.Products.Find(id);
+            }
+        }
+
+        // Db Read all
+        public static IEnumerable<Product> ListProducts()
+        {
+            using (var db = new Db())
+            {
+                return db.Products.ToList();
+            }
+        }
+
+        // Db Delete
+        public static void DeleteProduct(long id)
+        {
+            using (var db = new Db())
+            {
+                var product = db.Products.Find(id);
+                if (product != null)
+                {
+                    db.Products.Remove(product);
+                    db.SaveChanges();
+                }
+            }
+        }
+    }
+}
---
+++ Example 1/UnitTests/IntergrationTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using InventoryDb;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+//Install-Package Effort.EF6
+
+// Effort(Entity Framework Fake ObjectContext Realization Tool) is a third - party library
+// Creates a fake, in-memory context and can be used with EF 6 to provide in-memory testing capabilities.
+// Effort works by creating a transient, in-memory database that can mimic database operations without a physical database.
+
+namespace UnitTests
+{
+    [TestClass]
+    public class Crud_ProductsTests
+    {
+        private InventoryDb.Db context;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            // Create a new Effort connection for each test to ensure isolation
+            Effort.Provider.EffortConnection connection = Effort.DbConnectionFactory.CreateTransient();
+            context = new InventoryDb.Db(connection);
+        }
+
+        [TestMethod]
+        public void CreateUpdate_ProductDoesNotExist_AddsProduct()
+        {
+            // Arrange
+            var product = new InventoryDb.Product { Id = 0, Name = "New Product", Quantity = 5 };
+
+            // Act
+            Crud_Products.CreateUpdate(product, context);
+
+            // Assert
+            var addedProduct = context.Products.FirstOrDefault(p => p.Name == "New Product");
+            Assert.IsNotNull(addedProduct);
+            Assert.AreEqual(5, addedProduct.Quantity);
+        }
+
+        // Add other tests here...
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            context.Dispose();
+        }
+    }
+
+}
